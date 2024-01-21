b8bce17d3e286e0284d14ad2de64cc718b31b8d2
Xiang JiaHao <934622645@qq.com>
2024-01-21 15:38:48 +0800

    [soft] close segment display
    - remove log folder

--- xgpio_example/src/xgpio_example.c
+++ xgpio_example/src/xgpio_example.c
@@ -1,62 +1,61 @@
 /******************************************************************************
-* Copyright (C) 2002 - 2021 Xilinx, Inc.  All rights reserved.
-* Copyright (c) 2022 - 2023 Advanced Micro Devices, Inc. All Rights Reserved.
-* SPDX-License-Identifier: MIT
-******************************************************************************/
+ * Copyright (C) 2002 - 2021 Xilinx, Inc.  All rights reserved.
+ * Copyright (c) 2022 - 2023 Advanced Micro Devices, Inc. All Rights Reserved.
+ * SPDX-License-Identifier: MIT
+ ******************************************************************************/
 
 /*****************************************************************************/
 /**
-* @file xgpio_example.c
-*
-* This file contains a design example using the AXI GPIO driver (XGpio) and
-* hardware device.  It only uses channel 1 of a GPIO device and assumes that
-* the bit 0 of the GPIO is connected to the LED on the HW board.
-*
-*
-* <pre>
-* MODIFICATION HISTORY:
-*
-* Ver   Who  Date     Changes
-* ----- ---- -------- -----------------------------------------------
-* 1.00a rmm  03/13/02 First release
-* 1.00a rpm  08/04/03 Removed second example and invalid macro calls
-* 2.00a jhl  12/15/03 Added support for dual channels
-* 2.00a sv   04/20/05 Minor changes to comply to Doxygen and coding guidelines
-* 3.00a ktn  11/20/09 Minor changes as per coding guidelines.
-* 4.1   lks  11/18/15 Updated to use canonical xparameters and
-*		      clean up of the comments and code for CR 900381
-* 4.3   sk   09/29/16 Modified the example to make it work when LED_bits are
-*                     configured as an output. CR# 958644
-*       ms   01/23/17 Added xil_printf statement in main function to
-*                     ensure that "Successfully ran" and "Failed" strings
-*                     are available in all examples. This is a fix for
-*                     CR-965028.
-* 4.5  sne   06/12/19 Fixed IAR compiler warning.
-* 4.10 gm    07/11/23 Added SDT support.
-*
-* </pre>
-******************************************************************************/
+ * @file xgpio_example.c
+ *
+ * This file contains a design example using the AXI GPIO driver (XGpio) and
+ * hardware device.  It only uses channel 1 of a GPIO device and assumes that
+ * the bit 0 of the GPIO is connected to the LED on the HW board.
+ *
+ *
+ * <pre>
+ * MODIFICATION HISTORY:
+ *
+ * Ver   Who  Date     Changes
+ * ----- ---- -------- -----------------------------------------------
+ * 1.00a rmm  03/13/02 First release
+ * 1.00a rpm  08/04/03 Removed second example and invalid macro calls
+ * 2.00a jhl  12/15/03 Added support for dual channels
+ * 2.00a sv   04/20/05 Minor changes to comply to Doxygen and coding guidelines
+ * 3.00a ktn  11/20/09 Minor changes as per coding guidelines.
+ * 4.1   lks  11/18/15 Updated to use canonical xparameters and
+ *		      clean up of the comments and code for CR 900381
+ * 4.3   sk   09/29/16 Modified the example to make it work when LED_bits are
+ *                     configured as an output. CR# 958644
+ *       ms   01/23/17 Added xil_printf statement in main function to
+ *                     ensure that "Successfully ran" and "Failed" strings
+ *                     are available in all examples. This is a fix for
+ *                     CR-965028.
+ * 4.5  sne   06/12/19 Fixed IAR compiler warning.
+ * 4.10 gm    07/11/23 Added SDT support.
+ *
+ * </pre>
+ ******************************************************************************/
 
 /***************************** Include Files *********************************/
 
-#include "xparameters.h"
 #include "xgpio.h"
 #include "xil_printf.h"
-
+#include "xparameters.h"
 
 /************************** Constant Definitions *****************************/
 
-#define LED 0x01   /* Assumes bit 0 of GPIO is connected to an LED  */
-
+#define LED 0x01 /* Assumes bit 0 of GPIO is connected to an LED  */
+#define SEGMENT 0xff
 /*
  * The following constants map to the XPAR parameters created in the
  * xparameters.h file. They are defined here such that a user can easily
  * change all the needed parameters in one place.
  */
 #ifndef SDT
-#define GPIO_EXAMPLE_DEVICE_ID  XPAR_GPIO_0_DEVICE_ID
+#define GPIO_EXAMPLE_DEVICE_ID XPAR_GPIO_0_DEVICE_ID
 #else
-#define	XGPIO_AXI_BASEADDRESS	XPAR_XGPIO_1_BASEADDR
+#define XGPIO_AXI_BASEADDRESS XPAR_XGPIO_1_BASEADDR
 #endif
 
 /*
@@ -64,7 +63,7 @@
  * sure that it is visible to the human eye.  This constant might need to be
  * tuned for faster or slower processor speeds.
  */
-#define LED_DELAY     10000000
+#define LED_DELAY 10000000
 
 /*
  * The following constant is used to determine which channel of the GPIO is
@@ -73,7 +72,6 @@
 #define LED_CHANNEL 1
 
 /**************************** Type Definitions *******************************/
-
 
 /***************** Macros (Inline Functions) Definitions *********************/
 
@@ -84,22 +82,21 @@
  * uses an older version of the driver (pre 2.00a) which did not have a channel
  * parameter. Note that the channel parameter is fixed as channel 1.
  */
-#define XGpio_SetDataDirection(InstancePtr, DirectionMask) \
-	XGpio_SetDataDirection(InstancePtr, LED_CHANNEL, DirectionMask)
+#define XGpio_SetDataDirection(InstancePtr, DirectionMask)                     \
+  XGpio_SetDataDirection(InstancePtr, LED_CHANNEL, DirectionMask)
 
-#define XGpio_DiscreteRead(InstancePtr) \
-	XGpio_DiscreteRead(InstancePtr, LED_CHANNEL)
+#define XGpio_DiscreteRead(InstancePtr)                                        \
+  XGpio_DiscreteRead(InstancePtr, LED_CHANNEL)
 
-#define XGpio_DiscreteWrite(InstancePtr, Mask) \
-	XGpio_DiscreteWrite(InstancePtr, LED_CHANNEL, Mask)
+#define XGpio_DiscreteWrite(InstancePtr, Mask)                                 \
+  XGpio_DiscreteWrite(InstancePtr, LED_CHANNEL, Mask)
 
-#define XGpio_DiscreteSet(InstancePtr, Mask) \
-	XGpio_DiscreteSet(InstancePtr, LED_CHANNEL, Mask)
+#define XGpio_DiscreteSet(InstancePtr, Mask)                                   \
+  XGpio_DiscreteSet(InstancePtr, LED_CHANNEL, Mask)
 
 #endif
 
 /************************** Function Prototypes ******************************/
-
 
 /************************** Variable Definitions *****************************/
 
@@ -108,54 +105,64 @@
  * easily accessible from a debugger
  */
 
-XGpio Gpio; /* The Instance of the GPIO Driver */
+XGpio Gpio;         /* The Instance of the GPIO Driver */
+XGpio Gpio_segment; // segment
 
 /*****************************************************************************/
 /**
-*
-* The purpose of this function is to illustrate how to use the GPIO
-* driver to turn on and off an LED.
-*
-*
-* @return	XST_FAILURE to indicate that the GPIO Initialization had
-*		failed.
-*
-* @note		This function will not return if the test is running.
-*
-******************************************************************************/
-int main(void)
-{
-	int Status;
-	volatile int Delay;
-
-	/* Initialize the GPIO driver */
+ *
+ * The purpose of this function is to illustrate how to use the GPIO
+ * driver to turn on and off an LED.
+ *
+ *
+ * @return	XST_FAILURE to indicate that the GPIO Initialization had
+ *		failed.
+ *
+ * @note		This function will not return if the test is running.
+ *
+ ******************************************************************************/
+int main(void) {
+  int Status;
+  volatile int Delay;
+  //******************segment********************
+  // Initialize the 7 segment
+  XGpio_Initialize(&Gpio_segment, XPAR_XGPIO_0_BASEADDR);
+  // set output mode for GPIO
+  XGpio_SetDataDirection(&Gpio_segment, 1, ~SEGMENT);
+  // Set the SEGMENT to High for close segment
+  XGpio_DiscreteWrite(&Gpio_segment, 1, SEGMENT);
+  //******************segment********************
+  
+  /* Initialize the GPIO driver */
 #ifndef SDT
-	Status = XGpio_Initialize(&Gpio, GPIO_EXAMPLE_DEVICE_ID);
+  Status = XGpio_Initialize(&Gpio, GPIO_EXAMPLE_DEVICE_ID);
 #else
-	Status = XGpio_Initialize(&Gpio, XGPIO_AXI_BASEADDRESS);
+  Status = XGpio_Initialize(&Gpio, XGPIO_AXI_BASEADDRESS);
 #endif
-	if (Status != XST_SUCCESS) {
-		xil_printf("Gpio Initialization Failed\r\n");
-		return XST_FAILURE;
-	}
+  if (Status != XST_SUCCESS) {
+    xil_printf("Gpio Initialization Failed\r\n");
+    return XST_FAILURE;
+  }
 
-	/* Set the direction for all signals as inputs except the LED output */
-	XGpio_SetDataDirection(&Gpio, LED_CHANNEL, ~LED);
+  /* Set the direction for all signals as inputs except the LED output */
+  XGpio_SetDataDirection(&Gpio, LED_CHANNEL, ~LED);
 
-	/* Loop forever blinking the LED */
+  /* Loop forever blinking the LED */
 
-	while (1) {
-		/* Set the LED to High */
-		XGpio_DiscreteWrite(&Gpio, LED_CHANNEL, LED);
+  while (1) {
+    /* Set the LED to High */
+    XGpio_DiscreteWrite(&Gpio, LED_CHANNEL, LED);
 
-		/* Wait a small amount of time so the LED is visible */
-		for (Delay = 0; Delay < LED_DELAY; Delay++);
+    /* Wait a small amount of time so the LED is visible */
+    for (Delay = 0; Delay < LED_DELAY; Delay++)
+      ;
 
-		/* Clear the LED bit */
-		XGpio_DiscreteClear(&Gpio, LED_CHANNEL, LED);
+    /* Clear the LED bit */
+    XGpio_DiscreteClear(&Gpio, LED_CHANNEL, LED);
+    // XGpio_DiscreteClear(&Gpio_segment, 1, SEGMENT);
 
-		/* Wait a small amount of time so the LED is visible */
-		for (Delay = 0; Delay < LED_DELAY; Delay++);
-	}
-
+    /* Wait a small amount of time so the LED is visible */
+    for (Delay = 0; Delay < LED_DELAY; Delay++)
+      ;
+  }
 }
